0f719109f87aa85e297b9608ae33209de94ccc1a
tokeymoon <katsumatashota088@gmail.com>
2023-10-29 23:25:18 +0900

    コミット

--- blackjack.rb
+++ blackjack.rb
@@ -18,27 +18,24 @@
 class Deck
   attr_accessor :shuffle_deck, :s_rank
   def initialize
-    suits = ["ハート", "ダイアモンド", "クラブ", "スペード"]
+    @s_rank = []
+    @deck = []
+  end
+  def deckmake
     ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-
-    deck = []
-
+    suits = ["ハート", "ダイアモンド", "クラブ", "スペード"]
     suits.each do |suit|
       ranks.each do |rank|
         card = [suit, rank]
-        deck << card
+        @deck << card
       end
     end
-
-    @shuffle_deck = deck.shuffle
-
-    @s_rank = []
-
-    @shuffle_deck.each do |card|
-      suit, rank = card
-      @s_rank << rank
-    end
   end
+
+  def shuffle
+    @deck.shuffle
+  end
+
 end
 
 class Calculate
@@ -59,19 +56,21 @@ class DealerHand
   attr_accessor :deal_card, :dealer_hand, :n
   def initialize
     @dealer_hand = 0
-  @dealer_card = []
-  @n = 0
-  loop do
-  if @dealer_hand > 16
-    break
+    @dealer_card = []
+    @n = 0
   end
-    @dealer_hand = @dealer_hand + score(@s_rank[@n])
-    @dealer_card << @s_rank[@n]
-    if @dealer_hand > 21 && (@dealer_card[@n] == "A")
-        @dealer_hand = @dealer_hand -10
+  def loop
+    loop do
+      if @dealer_hand > 16
+        break
+      end
+      @dealer_hand = @dealer_hand + score(@s_rank[@n])
+      @dealer_card << @s_rank[@n]
+      if @dealer_hand > 21 && (@dealer_card[@n] == "A")
+          @dealer_hand = @dealer_hand -10
+      end
+      @n += 1
     end
-    @n += 1
-   end
   end
 end
 
@@ -81,31 +80,22 @@ class PlayerHand
     @player_hand = 0
     @player_card = []
     @m = 0
-loop do
-  if @player_hand > 16
-    break
   end
+  def loop
+  loop do
+    if @player_hand > 16
+      break
+    end
     @player_hand = @player_hand + score(@s_rank[51 - @m])
     @player_card << @s_rank[51 - @m]
     if @player_hand > 21 && (@player_card[51 - @m] == "A")
         @player_hand = @player_hand -10
     end
     @m += 1
-end
+    end
   end
 end
 
-# Deckクラスのインスタンスを生成
 deck = Deck.new
-
-# Calculateクラスを使用してスコアを計算
-calculator = Calculate.new
-puts calculator.score(deck.s_rank[0])
-
-# DealerHandクラスのインスタンスを生成
-dealer = DealerHand.new
-player = PlayerHand.new
-
-# インスタンス変数にアクセスしてディーラーの手札を表示
-puts dealer.dealer_hand
-puts dealer.dealer_card
+deck.deckmake
+p deck.shuffle

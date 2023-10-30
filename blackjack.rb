45cac9f51d0554aa3452967fdff9490d79a204dc
tokeymoon <146195365+tokeymoon@users.noreply.github.com>
2023-10-30 18:25:56 +0900

    Add files via upload
    kom

--- blackjack.rb
+++ blackjack.rb
@@ -1,120 +1,135 @@
-# ブラックジャックを開始します。
-# あなたの引いたカードはハートの7です。
-# あなたの引いたカードはクラブの8です。
-# ディーラーの引いたカードはダイヤのQです。
-# ディーラーの引いた2枚目のカードはわかりません。
-# あなたの現在の得点は15です。カードを引きますか？（Y/N）
-# Y
-# あなたの引いたカードはスペードの５です。
-# あなたの現在の得点は20です。カードを引きますか？（Y/N）
-# N
-# ディーラーの引いた2枚目のカードはダイヤの2でした。
-# ディーラーの現在の得点は12です。
-# ディーラーの引いたカードはハートのKです。
-# あなたの得点は20です。
-# ディーラーの得点は22です。
-# あなたの勝ちです！
-# ブラックジャックを終了します。]
 class Deck
-  attr_accessor 
   def initialize
-    @s_rank = []
     @deck = []
-    @pint = 0
-    @dint = 0
+    @count = 0
   end
-  def deckmake
+  def deck_make
     ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-    suits = ["ハート", "ダイアモンド", "クラブ", "スペード"]
+    suits = ["ハート", "ダイヤ", "クラブ", "スペード"]
     suits.each do |suit|
-      ranks.each do |rank|
-        card = [suit, rank]
-        @deck << card
-      end
+        ranks.each do |rank|
+            card = [suit, rank]
+            @deck << card
+        end
     end
-  end
-
-  def shuff
     @deck.shuffle!
   end
-
-  def pdraw(hand)
-      @pint += 1
-      hand.player_hand <<  @deck[@pint]
+  def draw(s, i, q)
+    @count += 1
+    s << @deck[@count]
+    suit, rank = @deck[@count]
+    i << rank
+    q << suit
   end
-
-  def ddraw(hand)
-      @dint -= 1
-      hand.dealer_hand << @deck[@dint]
+  def count
+    @count
   end
-
 end
 
 class Calculate
-  def score(i)
-    if i == "A"
-        11
-    elsif i.is_a?(String)
-        10
+  def initialize
+    @score = 0
+  end
+  def point(card)
+    @score = 0
+    card.each do |n|
+        if n == "A"
+            @score += 11
+        elsif n.is_a?(String)
+            @score += 10
+        else
+            @score += n.to_i
+        end
+    end
+    if @score > 21 && card.include?("A")
+        countA = card.count("A")
+        countA.times do
+          @score -= 10
+          break if @score < 22
+        end
+        @score
+      else
+        @score
+    end
+  end
+end
+
+class Judgement
+  def judge(a, b)
+    if a > b
+      p "あなたの勝ちです"
+    elsif b > a
+      p "ディーラーの勝ちです"
     else
-        i.to_i
+      p "引き分けです"
     end
   end
 end
 
-
-
-class DealerHand
-  attr_accessor :deal_card, :dealer_hand, :n
-  def initialize
-    @dealer_hand = []
-    @dealer_card = 0
-    @n = 0
-  end
-  def loop
-    loop do
-      if @dealer_hand > 16
-        break
-      end
-      @dealer_hand = @dealer_hand + score(@s_rank[@n])
-      @dealer_card << @s_rank[@n]
-      if @dealer_hand > 21 && (@dealer_card[@n] == "A")
-          @dealer_hand = @dealer_hand -10
-      end
-      @n += 1
-    end
-  end
-end
-
-class PlayerHand
-  attr_accessor :player_card, :player_hand, :m
-  def initialize
-    @player_hand = []
-    @player_score = 0
-    @m = 0
-  end
-  def loop
-  loop do
-    if @player_hand > 16
-      break
-    end
-    @player_hand = @player_hand + score(@s_rank[51 - @m])
-    @player_card << @s_rank[51 - @m]
-    if @player_hand > 21 && (@player_card[51 - @m] == "A")
-        @player_hand = @player_hand -10
-    end
-    @m += 1
-    end
-  end
-end
 
 deck = Deck.new
-phand = PlayerHand.new
-dhand = DealerHand.new
+calc = Calculate.new
+judge = Judgement.new
 
-deck.deckmake
-deck.shuff
-p deck.pdraw(phand)
-p deck.pdraw(phand)
-p deck.ddraw(dhand)
-p deck.ddraw(dhand)
+deck.deck_make
+
+player_hand = []
+player_card = []
+player_suit = []
+player_score = 0
+
+dealer_hand = []
+dealer_card = []
+dealer_suit = []
+dealer_score = 0
+
+deck.draw(player_hand, player_card, player_suit)
+deck.draw(player_hand, player_card, player_suit)
+deck.draw(dealer_hand, dealer_card, dealer_suit)
+deck.draw(dealer_hand, dealer_card, dealer_suit)
+
+player_score = calc.point(player_card)
+dealer_score = calc.point(dealer_card)
+
+p "ブラックジャックを開始します。"
+p "あなたの引いたカードは#{player_suit[0]}の#{player_card[0]}です。"
+p "あなたの引いたカードは#{player_suit[1]}の#{player_card[1]}です。"
+p "ディーラーの引いたカードは#{dealer_suit[0]}の#{dealer_card[0]}です。"
+p "ディーラーの引いた2枚目のカードはわかりません。"
+
+count = 1
+loop do
+  player_score = calc.point(player_card)
+  p "あなたの現在の得点は#{player_score}です。カードを引きますか？(Y/N)"
+  n = gets.chomp
+  if n == 'Y'
+    deck.draw(player_hand, player_card, player_suit)
+    count += 1
+    p "あなたの引いたカードは#{player_suit[count]}の#{player_card[count]}です。"
+  elsif n == 'N'
+    break
+  else
+    p '不明な入力です'
+  end
+end
+
+p "ディーラーの引いた2枚目のカードは#{dealer_suit[1]}の#{dealer_card[1]}でした。"
+dealer_count = 1
+loop do
+  dealer_score = calc.point(dealer_card)
+  p "ディーラーの現在の得点は#{dealer_score}です。"
+  if dealer_score > 16
+    break
+  else
+    deck.draw(dealer_hand, dealer_card, dealer_suit)
+    dealer_count += 1
+    p "ディーラーの引いたカードは#{dealer_suit[dealer_count]}の#{dealer_card[dealer_count]}でした。"
+  end
+end
+
+p "あなたの得点は#{player_score}です。"
+p "ディーラーの得点は#{dealer_score}です。"
+
+judge.judge(player_score, dealer_score)
+
+p "ブラックジャックを終了します。"
